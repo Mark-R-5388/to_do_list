1798f721bed08cf7e71643cc55393f8a62b1901c
unknown <marose5388@gmail.com>
2021-02-22 08:11:54 -0600

    functionality working

--- script.js
+++ script.js
@@ -3,15 +3,32 @@ const input = document.getElementById('input');
 const submitButton = document.getElementById('submit-btn');
 const list = document.getElementById('list');
 
-// Add to List
-submitButton.addEventListener('click', function (e) {
-  if (e.target === submitButton) {
-    let item = document.createElement('li');
+// Create Elements and Functionality
+function create() {
+  if (input.value.length > 0) {
+    let li = document.createElement('li');
     let deleteButton = document.createElement('button');
-    item.appendChild(document.createTextNode(input.value));
-    list.appendChild(item);
-    list.appendChild(deleteButton);
+    //add input to list
+    li.appendChild(document.createTextNode(input.value));
+    list.appendChild(li);
+    //add delete button to list
     deleteButton.innerHTML = 'Delete';
-    return (input.value = '');
+    li.appendChild(deleteButton);
+    input.value = '';
+    deleteButton.addEventListener('click', remove);
+    li.onclick = strikeThrough;
   }
-});
+}
+
+//Delete Button
+function remove(e) {
+  e.target.parentNode.remove();
+}
+
+//Submit Button
+submitButton.addEventListener('click', create);
+
+//Strike Through
+function strikeThrough(e) {
+  e.target.classList.toggle('complete');
+}
